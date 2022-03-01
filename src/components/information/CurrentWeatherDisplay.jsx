12aa1d61eca454f8bcedf0473851a57744ff5e0b
bucha789 <cursosdetuvida100@gmail.com>
2022-03-01 13:53:22 -0600

    agregando la opción de la localización y la configuración para el deploy

--- src/components/information/CurrentWeatherDisplay.jsx
+++ src/components/information/CurrentWeatherDisplay.jsx
@@ -1,22 +1,52 @@
 import React, { useContext, useState } from 'react';
-
+import { AppContext } from '../../context/AppContext';
+import { SearchPlacesContainer } from '../search/SearchPlacesContainer';
+import { WeatherImage } from './WeatherImage';
+import { types } from '../../types';
+import getWeekDates from '../../helpers/getWeekDates';
+import getWeatherData from '../../helpers/getWeatherData';
+import { setLocalStorage } from '../../helpers/getLocalStorage';
 import cloudBackground from '../../assets/images/Cloud-background.png';
 
 import '../../assets/styles/CurrentWeather.css';
-import { AppContext } from '../../context/AppContext';
-import getWeekDates from '../../helpers/getWeekDates';
-import { SearchPlacesContainer } from '../search/SearchPlacesContainer';
-import { WeatherImage } from './WeatherImage';
+
+
 
 export const CurrentWeatherDisplay = () => {
   const [showSearch, setShowSearch] = useState(false);
-  const [ state ] = useContext(AppContext);
+  const [ state, dispatch ] = useContext(AppContext);
   const { weatherStats, place } = state;
   const index = place.name.lastIndexOf(',');
   const name = place.name.slice(0, index)
   const {temp, weather} = weatherStats.current;
   const [description] = weather;
   const [todayDate] = getWeekDates()
+
+
+  const handleGetDataWithCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(({ coords }) => {
+      const place = {
+        name: 'Your Location,',
+        longitude: coords.longitude,
+        latitude: coords.latitude
+      }
+      getWeatherData(coords).then(data => {
+        const action = {
+          type: types.infoUdapte,
+          payload: {
+            place,
+            weatherStats: data
+          }
+        }
+        setLocalStorage('lastPlace', JSON.stringify(place));
+        dispatch(action)
+    }
+    );
+  });
+}
+
+
+
   return (
     <div className='current-weather__container'>
       {
@@ -25,7 +55,7 @@ export const CurrentWeatherDisplay = () => {
       }
       <div className='current-weather__controls'>
         <button onClick={() =>setShowSearch(true)}>Search for places</button>
-        <span>
+        <span onClick={handleGetDataWithCurrentLocation}>
           <i className='fas fa-bullseye'></i>
         </span>
       </div>
