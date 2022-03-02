a9a0e3e027492373b77580c1228e6c7255506bc7
bucha789 <cursosdetuvida100@gmail.com>
2022-03-02 15:36:58 -0600

    haciendo la configuracion nueva al getPlaces

--- src/helpers/getPlaces.js
+++ src/helpers/getPlaces.js
@@ -1,20 +1,7 @@
 export const getPlaces = (place) => {
   const URL_API =`https://proxy-google-mu.vercel.app/places/${place}`
   return fetch(URL_API)
-    .then((res) => res.json())
-    .then((data) => {
-      const { candidates } = data;
-      if (Array.isArray(candidates)) {
-        return candidates.map(item => {
-          return {
-            name: item.formatted_address,
-            latitude: item.geometry.location.lat,
-            longitude: item.geometry.location.lng
-          }
-        })
-      } else {
-        return []
-      }
-    })
-    .catch(console.log)
+  .then((res) => res.json())
+  .then((data) => data)
+  .catch(console.log)
 };
