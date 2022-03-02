0c503e40b9766df380c958991bf99616076e2ecf
bucha789 <cursosdetuvida100@gmail.com>
2022-03-02 11:02:33 -0600

    logs locos

--- api/findPlaces.js
+++ api/findPlaces.js
@@ -1,5 +1,6 @@
 
 export default async function findPlaces (req, res, place) {
+  console.log(place)
   const URL_API = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${encodeURI(place)}&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=AIzaSyDetjzC6BRLmDHiQZE-V7lM9j6Fph179uM`;
   return fetch(URL_API)
     .then(response => response.json())
