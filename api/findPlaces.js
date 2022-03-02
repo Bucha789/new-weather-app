da4f5a9c59a31fdd2b6187142ac61a4b692dba96
bucha789 <cursosdetuvida100@gmail.com>
2022-03-02 11:10:03 -0600

    segundo intento

--- api/findPlaces.js
+++ api/findPlaces.js
@@ -1,22 +1,26 @@
 
-export default async function findPlaces (req, res, place) {
-  console.log(place)
+export default function findPlaces (place) {
   const URL_API = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${encodeURI(place)}&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=AIzaSyDetjzC6BRLmDHiQZE-V7lM9j6Fph179uM`;
-  return fetch(URL_API)
-    .then(response => response.json())
-    .then(data => {
-      const { candidates } = data;
-      if (Array.isArray(candidates)) {
-        return res.send(candidates.map(item => {
-          return {
-            name: item.formatted_address,
-            latitude: item.geometry.location.lat,
-            longitude: item.geometry.location.lng
-          }
-        }))
-      } else {
-        return res.send([])
-      }
-    })
-    .catch(err => console.log(err))
+  console.log(place)
+  return function (req, res) {
+    return fetch(URL_API)
+      .then(response => response.json())
+      .then(data => {
+        const { candidates } = data;
+        if (Array.isArray(candidates)) {
+          return res.send(candidates.map(item => {
+            return {
+              name: item.formatted_address,
+              latitude: item.geometry.location.lat,
+              longitude: item.geometry.location.lng
+            }
+          }))
+        } else {
+          return res.send([])
+        }
+      })
+      .catch(err => console.log(err))
+  }
 }
+// export default async function findPlaces (req, res, place) {
+// }
